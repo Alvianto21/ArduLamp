293de17cdbb1c474f3cc82f33feab0792c592207
Alvianto21 <alviantovinsensius@gmail.com>
2025-07-18 19:20:31 +0700

    add dashboard page prepare button for lamp control

--- resources/views/gate/dashboard.blade.php
+++ resources/views/gate/dashboard.blade.php
@@ -1,4 +1,7 @@
 <x-homes.layout>
-	    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:title>{{ $title }}</x-slot:title>
 
+	<div class="d-flex gap-2 justify-content-center py-5">
+		<button type="button" class="btn btn-primary">Lamp</button>
+	</div>
 </x-homes.layout>
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('gate.dashboard', ['title' => 'Dashboard Page']);
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
